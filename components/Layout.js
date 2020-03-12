5486e3572fa0e300383c7c901b1c1513d2a14ebe
brohlson <chase@chaseohlson.com>
2020-03-12 02:29:59 -0700

    no intro

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
@@ -10,38 +10,12 @@ const Wrapper = styled.div`
   background: ${colors.white};
 `;
 
-const IntroIcon = styled.img.attrs({ alt: 'Icon' })`
-  width: ${({ started }) => (started ? '0%' : '250%')};
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  transition: all 1s ease;
-  z-index: 5;
-`;
-
 export default function Layout({ children }) {
-  const [run, setRun] = useState(false);
-  const [started, setStarted] = useState(false);
-  const [done, setDone] = useState(false);
-
-  useEffect(() => {
-    if (sessionStorage.getItem('introDone')) return setDone(true);
-    setRun(true);
-    setTimeout(() => {
-      setStarted(true);
-    }, 1000);
-    setTimeout(() => {
-      setDone(true);
-      sessionStorage.setItem('introDone', 'true');
-    }, 2000);
-  }, []);
   return (
     <Fragment>
       <RebootStyle />
       <GlobalStyle />
       <Favicon />
-      {!done && run && <IntroIcon src="icon.svg" started={started} />}
       <Wrapper>{children}</Wrapper>
     </Fragment>
   );
